427af89f770afc1cfef106bd5d57c6bcfa5beca3
Kaixin <48685463+GarudaID@users.noreply.github.com>
2022-01-30 22:00:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const config = {
-  Token: "",
+  Token: "OTI2NTAzOTc0NzQyNDA5MjQ2.Yc8oEQ.ifWSSEiuODhq5rZKxyc9OhGO1NQ",
   Default_Prefix: ".",
   Color: "#ff0000",
   Welcome_Images: "https://3.bp.blogspot.com/-rHGW-Pj086E/XJqrXq3DJlI/AAAAAAAABB0/kRGUxP_OqkUmicF3bW129ubAa0myCJs9ACKgBGAs/w4096-h2304-c/sunset-horizon-scenery-landscape-art-uhdpaper.com-4K-178.jpg",
